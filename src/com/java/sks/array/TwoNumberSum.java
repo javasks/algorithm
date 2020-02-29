3ebe60136ff6a895acfad8390d75b58aa522dcd2
sandeep <ankurmall89@gmail.com>
2020-02-29 17:45:20 +0530

    Added TwoNumber sum

--- src/com/java/sks/array/TwoNumberSum.java
+++ src/com/java/sks/array/TwoNumberSum.java
@@ -1,4 +1,24 @@
 package com.java.sks.array;
 
 public class TwoNumberSum {
+
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] + array[j] == targetSum) {
+                    return new int[]{array[i], array[j]};
+                }
+
+            }
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        int A[] = {1, 4, 45, 6, 10, -8};
+        int[] array = twoNumberSum(A, 16);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
 }
