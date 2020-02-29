191df410f7f62c8862ec79b7e40ff9c751986af9
sandeep <ankurmall89@gmail.com>
2020-02-29 18:14:17 +0530

    Added TwoNumber sum

--- src/com/java/sks/array/TwoNumberSum.java
+++ src/com/java/sks/array/TwoNumberSum.java
@@ -1,8 +1,15 @@
 package com.java.sks.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author - Sandeep Singh
+ */
+
 public class TwoNumberSum {
 
-    public static int[] twoNumberSum(int[] array, int targetSum) {
+    public static int[] twoNumberSumON2(int[] array, int targetSum) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] + array[j] == targetSum) {
@@ -14,9 +21,23 @@ public class TwoNumberSum {
         return new int[0];
     }
 
+    public static int[] twoNumberSumONON(int[] array, int targetSum) {
+        Map<Integer,Boolean> hashMap=new HashMap<>();
+        for(int localNumber: array){
+            int potentialSum=targetSum-localNumber;
+            if(hashMap.containsKey(potentialSum)){
+                return new int[] {potentialSum,localNumber};
+            }
+            hashMap.put(localNumber,true);
+        }
+
+
+        return new int[0];
+    }
+
     public static void main(String[] args) {
         int A[] = {1, 4, 45, 6, 10, -8};
-        int[] array = twoNumberSum(A, 16);
+        int[] array = twoNumberSumONON(A, 16);
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
