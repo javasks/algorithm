e34050158fe0ac9c40cfb5791c752cc0f71738e5
sandeep <ankurmall89@gmail.com>
2020-03-20 11:27:51 +0530

    added eggdrop problem top down approach

---
+++ src/com/java/sks/dp/eggDrop/TopDownEggDrop.java
@@ -0,0 +1,94 @@
+package com.java.sks.dp.eggDrop;
+
+
+/*
+@Author Sandeep Singh
+ */
+
+class TopDownEggDrop {
+
+    /**
+     * @param totalEggs
+     * @param totalFloors
+     * @return
+     */
+
+    public static int eggDrop(int totalEggs, int totalFloors) {
+        int[][] eggsAndFloorsMemory = new int[totalEggs + 1][totalFloors + 1];
+        initializeMemory(totalEggs, totalFloors, eggsAndFloorsMemory);
+
+        return eggDrop(totalEggs, totalFloors, eggsAndFloorsMemory);
+    }
+
+    /**
+     * @param totalEggs
+     * @param totalFloors
+     * @param eggsAndFloorsMemory
+     * @return
+     */
+    private static int eggDrop(int totalEggs, int totalFloors, int[][] eggsAndFloorsMemory) {
+        /**
+         *
+         * Base Case 1:
+         * If we have 0 floors we need 0 trials, no matter the egg amount given
+         * If we have 1 floor we need 1 trial, no matter the egg amount given
+         *
+         */
+        if (totalFloors == 1 || totalFloors == 0) {
+            return totalFloors;
+        }
+
+        /**
+         * If we have 1 egg. We will have to be try every floor
+         * 1 by 1 starting from floor 1 all the way up to totalFloors
+         * This entails 'totalFloors' drops as the BEST we can do to guarantee
+         * we find the pivotal floor
+         */
+        if (totalEggs == 1) {
+            return totalFloors;
+        }
+
+        /**
+         * Do we already know the answer to this subproblem?
+         */
+        if (eggsAndFloorsMemory[totalEggs][totalFloors] != Integer.MAX_VALUE) {
+            return eggsAndFloorsMemory[totalEggs][totalFloors];
+        }
+
+
+        /**
+         *  Try all floors up to the floor we are working on. See the bottom explanation
+         *  below for how the situations break down
+         */
+        for (int floor = 1; floor <= totalFloors; floor++) {
+
+            int costOfWorstOutcome = Math.max(eggDrop(totalEggs - 1, floor - 1, eggsAndFloorsMemory),
+                    eggDrop(totalEggs, totalFloors - floor, eggsAndFloorsMemory));
+
+            int accountingForDroppingAtThisSubproblem = 1 + costOfWorstOutcome;
+            eggsAndFloorsMemory[totalEggs][totalFloors] = Math.min(eggsAndFloorsMemory[totalEggs][totalFloors],
+                    accountingForDroppingAtThisSubproblem);
+        }
+
+        return eggsAndFloorsMemory[totalEggs][totalFloors];
+    }
+
+    /*
+      Initialize all subproblems that need to be solved. This does not change asymptotic
+      complexity of the solution since the work stays within our upper bound.
+      Not sure if we really need to do this...eh...whatever you get the point
+    */
+    private static void initializeMemory(int totalEggs, int totalFloors, int[][] cache) {
+        for (int eggs = 2; eggs <= totalEggs; eggs++) {
+            for (int floor = 2; floor <= totalFloors; floor++) {
+                cache[eggs][floor] = Integer.MAX_VALUE;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        // TopDownEggDrop topDownEggDrop = new TopDownEggDrop();
+        System.out.println(eggDrop(3, 6));
+    }
+}
